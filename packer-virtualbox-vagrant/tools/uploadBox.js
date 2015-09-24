d68964316514af1c169323abb30953c7a174bb8d
Emmanuel Kasper <e.kasper@proxmox.com>
2015-09-24 13:50:45 +0200

    Add formatting output

--- packer-virtualbox-vagrant/tools/uploadBox.js
+++ packer-virtualbox-vagrant/tools/uploadBox.js
@@ -5,6 +5,7 @@ helper to upload vagrant base boxes to atlas
 
 boxes hosted on atlas have the following schema:
 accountName/codename/version/provider
+CODENAME=wheezy64 VERSION=7.9.0 tools/uploadBox.js
 
 which means we have to 
  * create first our account (debian)
@@ -69,4 +70,17 @@ var uploadVersionProvider = function(version) {
 	return xhr.responseText;
 };
 
-console.log(checkStatus(codename,version));
+var displayStatus = function(codename, version) {
+	var status = JSON.parse(checkStatus(codename, version));
+	if (status.created_at) {
+		console.log('codename: ' + codename);
+		console.log('version: ' + version);
+		console.log('created_at: ' + status.created_at);
+		console.log('download_url: ' + status.download_url);
+
+	} else {
+		console.log(codename + ' ' + version + ' not found');
+	}
+}
+
+displayStatus(codename, version);
