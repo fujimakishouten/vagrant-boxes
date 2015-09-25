47a48584e8be39e28ca5a4d461c731e9d9580e68
Emmanuel Kasper <e.kasper@proxmox.com>
2015-09-25 15:46:24 +0200

    only update checksum if necessary, basic error handling, sort json file when written

--- packer-virtualbox-vagrant/tools/updateTestingIso.pl
+++ packer-virtualbox-vagrant/tools/updateTestingIso.pl
@@ -8,23 +8,14 @@ use LWP::Simple;
 
 my $url = 'http://cdimage.debian.org/cdimage/daily-builds/daily/arch-latest/amd64/iso-cd/SHA512SUMS';
 my $file = '/tmp/packer-virtualbox-vagrant-SHA512SUMS';
-
 getstore($url, $file);
-
-open (DATA, "< $file");
-
-my $content;
-
-while (my $line = <DATA>) {
-	$content .= $line;
-}
-
+open (DATA, "< $file") or die "checksum file $file not found";
+my @lines = <DATA>;
 close DATA;
 unlink $file;
-
 # get the iso checksum by stripping the iso name
 # iso name is expressed as a suit of blank spaces, one word and a carriage return
-my $isoSum = $content =~ s/\s*\S+\n//r;
+my $isoSum = $lines[0] =~ s/\s*\S+\n//r;
 
 my $json;
 {
@@ -36,17 +27,22 @@ my $json;
 
 my $manifest = decode_json($json);
 
-print "old iso_checksum: ".$manifest->{'builders'}->[0]->{'iso_checksum'}."\n";
-$manifest->{'builders'}->[0]->{'iso_checksum'} = $isoSum;
-$manifest->{'builders'}->[0]->{'iso_checksum_type'} = 'sha512';
-print "new iso_checksum: ".$isoSum."\n";
+my $old_iso_checksum = $manifest->{'builders'}->[0]->{'iso_checksum'};
 
-open my $fh, ">", "debian-9-testing-virtualbox.json";
-print $fh JSON->new->pretty->encode($manifest)."\n";
-close $fh;
+if ($isoSum eq $old_iso_checksum) {
+	print "checksum $old_iso_checksum is up to date, doing nothing\n";
+	exit 0;
+}else {
+	print "old iso_checksum: $old_iso_checksum\n";
+	$manifest->{'builders'}->[0]->{'iso_checksum'} = $isoSum;
+	print "new iso_checksum: ".$isoSum."\n";
 
+	open my $fh, ">", "debian-9-testing-virtualbox.json";
+	my $json_printer = JSON->new;
+	$json_printer = $json_printer->canonical(1);
+	$json_printer = $json_printer->pretty(1);
+	print $fh $json_printer->encode($manifest)."\n";
+	close $fh;
 
-
-
-	
-	
+	exit 0;
+}
