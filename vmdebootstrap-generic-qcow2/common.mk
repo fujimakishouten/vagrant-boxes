d50a9aebdb21297b2599316e529a3c95945a5375
Antonio Terceiro <terceiro@debian.org>
2016-04-24 12:42:45 -0300

    vmdebootstrap-generic-qcow2: split common rules from main Makefile

---
+++ vmdebootstrap-generic-qcow2/common.mk
@@ -0,0 +1,34 @@
+ARCH = $(shell dpkg --print-architecture)
+HTTP_PROXY_SETTING := $(shell nc -z -w 0 127.0.0.1 3142 && echo 'http_proxy=http://127.0.0.1:3142')
+
+.PRECIOUS: %.qcow2 %.qcow2.gz
+
+%.gz: %
+	(cat $< | gzip -) > $@ || ($(RM) $@; false)
+
+%.qcow2: %.raw
+	qemu-img convert -O qcow2 $< $@
+
+%.raw:
+	sudo \
+		$(HTTP_PROXY_SETTING) \
+		vmdebootstrap \
+		$(VMDEBOOTSTRAP_OPTIONS) \
+		--verbose \
+		--size 10G \
+		--distribution $* \
+		--arch $(ARCH) \
+		--package openssh-server \
+		--package sudo \
+		--serial-console \
+		--hostname $* \
+		--enable-dhcp \
+		--log $*.log \
+		--image $@ \
+		|| (sudo $(RM) $@; false)
+	sync
+	sudo chown $(USER) $@ $*.log
+
+clean::
+	$(RM) *.raw *.qcow2 *.gz *.log
+
