2bc1ce432246ab713d3921f1de4a0dc66ec94e6c
Emmanuel Kasper <emmanuel@libera.cc>
2016-06-11 20:40:43 +0200

    vmdebootstrap-virtualbox-vagrant: add functional testing and vagrant helper

---
+++ helpers/vagrantBoxTesting.mk
@@ -0,0 +1,14 @@
+define functest
+ @ # $< is the fist dependency of the target
+ @echo functional testing for $< box
+ @vagrant box add --name $1 $2
+ @vagrant init $1
+ @vagrant up
+ @vagrant ssh -c "sudo apt-get update"
+ @vagrant ssh -c "sudo apt-get --quiet --yes install linuxlogo && linuxlogo"
+ @vagrant ssh -c "test -f /vagrant/Makefile && echo -e $(tput bold) syncing successfull !"
+ @vagrant halt
+ @vagrant destroy --force
+ @vagrant box remove $1
+ @rm Vagrantfile
+endef
