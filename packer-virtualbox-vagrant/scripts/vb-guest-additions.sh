9d42699db3bcc9b637982d7e9ed7c3e575d46d58
Emmanuel Kasper <emmanuel@libera.cc>
2016-06-17 12:05:24 +0200

    packer-virtualbox-vagrant: rename vb-guest-additions.sh and make it use debian contrib repo

---
+++ packer-virtualbox-vagrant/scripts/vb-guest-additions.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+echo "installing virtualbox guest additions"
+sed -i 's/main$/main contrib/' /etc/apt/sources.list
+apt-get update
+
+# install virtualbox additions build dependancies
+apt-get --yes install --no-install-recommends linux-headers-amd64 virtualbox-guest-dkms
+
+# cleanup virtualbox stuff
+apt-get --yes remove linux-headers-amd64
