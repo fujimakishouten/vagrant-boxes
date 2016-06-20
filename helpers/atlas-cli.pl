e68db94712bf7f5b6896b0f009b3aea303fcbf84
Emmanuel Kasper <emmanuel@libera.cc>
2016-06-20 19:41:33 +0200

    helpers: reindent atlas-cli with emacs similar indenting

--- helpers/atlas-cli.pl
+++ helpers/atlas-cli.pl
@@ -24,15 +24,17 @@ my $end_point = 'https://atlas.hashicorp.com/api/v1/box/debian';
 my $ua = LWP::UserAgent->new;
 my $JSONprinter = JSON->new()->canonical->pretty;
 my $verbose = 1;
+my $debug = 0;
 
 sub main {
-    my $codename = $ARGV[0] // 'testing64';
+    my $codename = $ARGV[0] // 'testing';
     my $manifest =join('', $builder_dir, '/', $codename, '.json');
     my $version = json_fileread('box_version', $manifest);
     my $version_log = json_fileread('box_changelog', $manifest);
     my $provider = 'virtualbox';
+    my $cloudname = join('', $codename, '64');
 
-#	delver() && exit(0);
+#	delver($cloudname, $version) && exit(0);
 #	createver();
 #	checkver();
 #	update();
@@ -41,132 +43,134 @@ sub main {
 #	get_uploadpath();
 #	uploadbox(get_uploadpath());
 
-    createver($codename, $version, $version_log)
-	&& createprovider($codename, $version, $provider)
-	&& uploadbox(get_uploadpath($codename, $version, $provider), $codename);
+    createver($cloudname,  $version, $version_log)
+	&& createprovider($cloudname, $version, $provider)
+	&& uploadbox(get_uploadpath($cloudname, $version, $provider), $codename);
 }
 
 main();
 
 sub json_fileread {
-	my ($key, $template) = @_;
-	my $json;
+    my ($key, $template) = @_;
+    my $json;
 
-	{
-		local $INPUT_RECORD_SEPARATOR = undef;
-		open (my $fh, "<", $template) or die "could not find $template";
-		$json = <$fh>;
-		close $fh;
-	}
+    {
+	local $INPUT_RECORD_SEPARATOR = undef;
+	open (my $fh, "<", $template) or die "could not find $template";
+	$json = <$fh>;
+	close $fh;
+    }
 
-	my $manifest = decode_json($json);
-	my $value = $manifest->{'variables'}->{$key} if $manifest->{'variables'}->{$key};
-	defined($value) && return $value or die "unable to find $key in $template";
+    my $manifest = decode_json($json);
+    my $value = $manifest->{'variables'}->{$key} if $manifest->{'variables'}->{$key};
+    defined($value) && return $value or die "unable to find $key in $template";
 }
 
 sub printJSON {
-	my ($response, $debug) = @_;
-	
-	if (defined $debug) {
-		print $response->status_line(), "\n";
-		print Dumper($response);
-		return;
-	}	
+    my ($response) = @_;
+    if ($debug) {
+	print $response->status_line(), "\n";
+	print Dumper($response);
+    return;
+    }
 
-	if ($response->is_success) {
-			if ($verbose) {
-				my $jsonref = decode_json($response->decoded_content);
-				print $JSONprinter->encode($jsonref);
-			} else {
-				print $response->status_line(), "\n";
-			} 
+    if ($response->is_success) {
+	if ($verbose) {
+	    my $jsonref = decode_json($response->decoded_content);
+	    print $JSONprinter->encode($jsonref);
 	} else {
-		die $response->status_line();
+	    print $response->status_line(), "\n";
 	}
+	} else {
+	die $response->status_line();
+    }
 }
 
 sub checkver {
-	my ($codename, $version) = @_;
-	my $url = join('/', $end_point, $codename, "version", "$version?access_token=$atlas_token");
-	my $response = $ua->get($url);
-	printJSON($response);
+    my ($codename, $version) = @_;
+    my $url = join('/', $end_point, $codename, "version", "$version?access_token=$atlas_token");
+    my $response = $ua->get($url);
+    printJSON($response);
 }
 
 sub createver {
-	my ($codename, $version, $version_log) = @_;
-	my $url = join('/', $end_point, $codename, "versions");
-	my	$response = $ua->post($url, 
-		[
-			'version[version]' => "$version",
-			'version[description]' => "$version_log",
-			'access_token' => "$atlas_token"
-		]
-	);
-	printJSON($response);
-	return $response->is_success;
+    my ($codename, $version, $version_log) = @_;
+    my $url = join('/', $end_point, $codename, "versions");
+    my $response = $ua->post($url,
+	[
+	    'version[version]' => "$version",
+	    'version[description]' => "$version_log",
+	    'access_token' => "$atlas_token"
+	]
+    );
+    printJSON($response);
+    return $response->is_success;
 }
 
 sub delver {
-	my ($codename, $version) = @_;
-	my $url = join('/', $end_point, $codename, "version", $version);
-	
-	# Arguments in LWP::UserAgent::delete() are used to create headers not content. 
-	# So Use HTTP:Request for that
-	my $headers = HTTP::Headers->new('content-type' => 'application/x-www-form-urlencoded');
-	my $content = "access_token=$atlas_token";
-	my $req = HTTP::Request->new(DELETE => $url, $headers, $content);
+    my ($codename, $version) = @_;
+    my $url = join('/', $end_point, $codename, "version", $version);
 
-	my $response = $ua->request($req);
-	printJSON($response);
-	return $response->is_success;
+    # Arguments in LWP::UserAgent::delete() are used to create headers not content. 
+    # So Use HTTP:Request for that
+    my $headers = HTTP::Headers->new('content-type' => 'application/x-www-form-urlencoded');
+    my $content = "access_token=$atlas_token";
+    my $req = HTTP::Request->new(DELETE => $url, $headers, $content);
+
+    my $response = $ua->request($req);
+    printJSON($response);
+    return $response->is_success;
 }
 
 sub update {
-	my ($codename, $version, $version_log) = @_;
-	my $url = join('/', $end_point, $codename, "version", $version);
-	my	$response = $ua->put($url, 
-		[
-			'version[version]' => "$version",
-			'version[description]' => "$version_log",
-			'access_token' => "$atlas_token"
-		]
-	);
-	printJSON($response);
-	return $response->is_success;
+    my ($codename, $version, $version_log) = @_;
+    my $url = join('/', $end_point, $codename, "version", $version);
+    my $response = $ua->put($url,
+	[
+	    'version[version]' => "$version",
+	    'version[description]' => "$version_log",
+	    'access_token' => "$atlas_token"
+	]
+    );
+    printJSON($response);
+    return $response->is_success;
 }
 
 sub createprovider {
-	my ($codename, $version, $provider) = @_;
-	my $url = join('/', $end_point, $codename, "version", $version, "providers");
-	my	$response = $ua->post($url, 
-		[ 'provider[name]', "$provider", 'access_token', "$atlas_token"]
-	);
+    my ($codename, $version, $provider) = @_;
+    my $url = join('/', $end_point, $codename, "version", $version, "providers");
+    my $response = $ua->post($url, 
+	[
+	    'provider[name]', "$provider",
+	    'access_token', "$atlas_token"
+	]
+    );
 	printJSON($response);
 	return $response->is_success;
 }
 
 sub checkprovider {
-	my ($codename, $version, $provider) = @_;
-	my $url = join('/', $end_point, $codename, "version", $version, "provider", $provider, "?access_token=$atlas_token");
-	my $response = $ua->get($url);
-	printJSON($response);
-	return $response->is_success;
+    my ($codename, $version, $provider) = @_;
+    my $url = join('/', $end_point, $codename, "version", $version, "provider", $provider, "?access_token=$atlas_token");
+    my $response = $ua->get($url);
+    printJSON($response);
+    return $response->is_success;
 }
 
 sub get_uploadpath {
-	my ($codename, $version, $provider) = @_;
-	my $url = join('/', $end_point, $codename, "version", $version, "provider", $provider, "upload", "?access_token=$atlas_token");
-	my $response = $ua->get($url);
-	printJSON($response);
-	
-	my $jsonref = decode_json($response->decoded_content);
-	return $jsonref->{'upload_path'};
+    my ($codename, $version, $provider) = @_;
+    my $url = join('/', $end_point, $codename, "version", $version, "provider", $provider, "upload", "?access_token=$atlas_token");
+    my $response = $ua->get($url);
+    printJSON($response);
+
+    my $jsonref = decode_json($response->decoded_content);
+    return $jsonref->{'upload_path'};
 }
 
 sub uploadbox {
-	my ($url, $codename) = @_;
-	my $curl = "curl -X PUT $url --upload-file $codename.box";
-	$OUTPUT_AUTOFLUSH = 1;
+    my ($url, $codename) = @_;
+    my $curl = "curl -X PUT $url --upload-file $codename.box";
+    $OUTPUT_AUTOFLUSH = 1;
 
     open(CURL,  '-|', $curl) or die "error: $ERRNO";
     while (<CURL>) { say; }
