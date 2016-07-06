1ba54c130d8bcbda56f46f261b1a3b3119f006ee
Emmanuel Kasper <emmanuel@libera.cc>
2016-07-06 19:39:45 +0200

    packer-virtualbox-vagrant: output all all commands to a log file via adding tee(1) at the end of each line

--- helpers/vagrantBoxTesting.mk
+++ helpers/vagrantBoxTesting.mk
@@ -1,17 +1,17 @@
 define functest
  @ # $< is the fist dependency of the target
  @echo functional testing for $< box
- @vagrant box add --name $1 $2
- @vagrant init $1
- @vagrant up
+ @vagrant box add --name $1 $2 $(TEE)
+ @vagrant init $1 $(TEE)
+ @vagrant up $(TEE)
  @vagrant ssh -c "sudo apt-get --quiet --yes install figlet \
-     && ( grep ^ID /etc/os-release; cat /etc/debian_version) | figlet"
+     && ( grep ^ID /etc/os-release; cat /etc/debian_version) | figlet" $(TEE)
  @vagrant ssh -c "test -f /vagrant/Makefile \
-     && echo -e $(tput bold) syncing successfull !"
- @vagrant halt
- @vagrant destroy --force
- @vagrant box remove $1
- @rm Vagrantfile
+     && echo -e $(tput bold) syncing successfull !" $(TEE)
+ @vagrant halt $(TEE)
+ @vagrant destroy --force $(TEE)
+ @vagrant box remove $1 $(TEE)
+ @rm Vagrantfile $(TEE)
 endef
 
 define forcecleantest
