926274c87bcf46b45011bfb6c8a7940bef3a6a96
Emmanuel Kasper <emmanuel@libera.cc>
2016-09-22 19:20:02 +0200

    add --provider when calling ithe  tests so we can use them with other providers

--- helpers/vagrantBoxTesting.mk
+++ helpers/vagrantBoxTesting.mk
@@ -1,17 +1,17 @@
 define functest
  @echo functional testing for $< box
- @vagrant box add --name $1 $2 $(TEE)
+ @vagrant box add --name $1 $2 --provider $3 $(TEE)
  @ # this will also test that sync is working
  @cp ../helpers/package_report . $(TEE)
  @vagrant init $1 $(TEE)
- @vagrant up $(TEE)
+ @vagrant up --provider $3 $(TEE)
  @vagrant ssh -c "sudo apt-get --quiet --yes install figlet \
      && ( lsb_release --release --codename ) | figlet" $(TEE)
  @vagrant ssh -c "sh /vagrant/package_report" $(TEE)
  @$(RM) package_report $(TEE)
  @vagrant halt $(TEE)
  @vagrant destroy --force $(TEE)
- @vagrant box remove $1 $(TEE)
+ @vagrant box remove $1 --provider $3 $(TEE)
  @rm Vagrantfile $(TEE)
 endef
 
@@ -19,5 +19,5 @@ define forcecleantest
 	-vagrant halt --force
 	-vagrant destroy --force
 	-$(RM) Vagrantfile
-	-vagrant box remove $1
+	-vagrant box remove $1 --provider $2
 endef
