7a79320ad07cbfdba5e4d2868693a1cfd613ec71
Emmanuel Kasper <emmanuel@libera.cc>
2016-10-15 03:00:45 +0200

    virt-install-generic-raw: multiple fixes
    * do no start a serial console: virt-install makes the machine always reboot
    * use httpredir for mirrors
    * noautoconsole so the build process is daemonized

--- virt-install-generic-raw/build.sh
+++ virt-install-generic-raw/build.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-python -m SimpleHTTPServer &
-child_pid=$!
-
 export OS=Debian8
 
 virt-install \
@@ -12,11 +9,9 @@ virt-install \
 --vcpus 1 \
 --file ${OS}.img \
 --file-size=4 \
---location http://http.debian.net/debian/dists/stable/main/installer-amd64/ \
+--location http://httpredir.debian.org/debian/dists/stable/main/installer-amd64/ \
 --virt-type kvm \
 --os-variant Debian8 \
 --network=user \
---console pty,target_type=serial \
---graphics none \
---extra-args "auto=true hostname=${OS} domain= url=http://10.0.2.2:8000/vanilla-debian-8-jessie-preseed.cfg text console=ttyS0,115200n8 serial TERM=bterm"
-kill $child_pid
+--noautoconsole \
+--extra-args "auto=true hostname=${OS} domain= url=http://10.0.2.2:8000/vanilla-debian-8-jessie-preseed.cfg"
