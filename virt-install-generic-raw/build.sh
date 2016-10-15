d29601673cb458af2004cd13f17fd0400dc70282
Emmanuel Kasper <emmanuel@libera.cc>
2016-10-15 10:47:01 +0200

    virt-install-generic-raw: force shutdown on install end, use serial console

--- virt-install-generic-raw/build.sh
+++ virt-install-generic-raw/build.sh
@@ -1,17 +1,27 @@
-#!/bin/sh
+#!/bin/bash
+# MN: trap signal has bash semantics
 
 export OS=Debian8
+
+python -m SimpleHTTPServer & 2> /dev/null
+child_pid=$!
 
 virt-install \
 --connect qemu:///session \
 --name ${OS} \
 --ram 512 \
 --vcpus 1 \
---file ${OS}.img \
+--file ${OS}.qcow2 \
 --file-size=4 \
 --location http://httpredir.debian.org/debian/dists/stable/main/installer-amd64/ \
 --virt-type kvm \
 --os-variant Debian8 \
 --network=user \
---noautoconsole \
---extra-args "auto=true hostname=${OS} domain= url=http://10.0.2.2:8000/vanilla-debian-8-jessie-preseed.cfg"
+--noreboot \
+--graphics none \
+--console pty,target_type=serial \
+--extra-args "auto=true hostname=${OS} domain= url=http://10.0.2.2:8000/vanilla-debian-8-jessie-preseed.cfg console=ttyS0,115200n8 DEBIAN_FRONTEND=text"
+
+kill $child_pid
+
+trap "kill $child_pid" SIGINT SIGTERM
