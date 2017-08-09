4ff402a7745fa7a0c3000c9b2fc46805a4846185
Lucas Nussbaum <lucas@debian.org>
2017-08-09 05:46:46 +0200

    packer-virtualbox-vagrant: update testing image, and introduce a script to update from stretch

---
+++ packer-virtualbox-vagrant/scripts/upgrade-to-testing.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -e
+cat > /etc/apt/sources.list <<-EOF
+deb http://deb.debian.org/debian testing main
+deb-src http://deb.debian.org/debian testing main
+
+deb http://deb.debian.org/debian sid main
+deb-src http://deb.debian.org/debian sid main
+EOF
+
+cat > /etc/apt/preferences <<-EOF
+Package: *
+Pin: release a=testing
+Pin-Priority: 900
+
+Package: *
+Pin: release a=unstable
+Pin-Priority: 800
+EOF
+
+apt-get update
+apt-get --yes upgrade
+apt-get --yes dist-upgrade
+apt-get --yes autoremove
+apt-get --yes clean
