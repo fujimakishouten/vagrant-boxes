671c8983da4a8594a9e3eb17572965d5b842bf46
Lucas Nussbaum <lucas@debian.org>
2017-08-25 05:32:24 -0700

    [packer-virtualbox-vagrant] improve cleanup (remove kernel headers)

--- packer-virtualbox-vagrant/scripts/vb-guest-additions-stretch.sh
+++ packer-virtualbox-vagrant/scripts/vb-guest-additions-stretch.sh
@@ -13,8 +13,7 @@ wget http://ftp.debian.org/debian/pool/contrib/v/virtualbox/virtualbox-guest-uti
 dpkg -i *deb || true
 apt-get --yes -f --no-install-recommends install
 
-apt-get --yes remove linux-headers-amd64
-# FIXME cleanup doesnt really work as expected
+apt-get --yes remove --auto-remove linux-headers*-amd64
 apt-get --yes autoremove
 apt-get --yes clean
 cd /
