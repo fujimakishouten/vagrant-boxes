a8fc978b3a8b319bf59fe2821033a1d1a41c764d
Emmanuel Kasper <manu@debian.org>
2019-01-25 22:57:20 +0100

    packer-virtualbox-vagrant: determine virtualbox packages to install for stretch via rmadison
    a bit overkill, but works for the momment

--- packer-virtualbox-vagrant/scripts/vb-guest-additions-stretch.sh
+++ packer-virtualbox-vagrant/scripts/vb-guest-additions-stretch.sh
@@ -3,17 +3,20 @@
 set -e
 
 echo "installing virtualbox guest additions"
+apt-get --yes install linux-headers-amd64 devscripts
 
-VBOXVER=5.2.18-dfsg-2~bpo9+1
+VBOXVER=$(rmadison --suite stretch-backports virtualbox-guest-dkms \
+	| awk -F'|' '{print $2}' \
+	| sed s/\ //g)
+
 td=$(mktemp -d)
 cd $td
-apt-get --yes install linux-headers-amd64
 wget http://ftp.debian.org/debian/pool/contrib/v/virtualbox/virtualbox-guest-dkms_${VBOXVER}_all.deb
 wget http://ftp.debian.org/debian/pool/contrib/v/virtualbox/virtualbox-guest-utils_${VBOXVER}_amd64.deb
 dpkg -i *deb || true
 apt-get --yes -f --no-install-recommends install
 
-apt-get --yes remove --auto-remove linux-headers*-amd64
+apt-get --yes remove --auto-remove linux-headers*-amd64 devscripts
 apt-get --yes autoremove
 apt-get --yes clean
 cd /
