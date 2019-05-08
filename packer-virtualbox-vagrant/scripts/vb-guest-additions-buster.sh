f1c2ba8a459f0c1138ec9854c86e7071f3d0dbd7
Lucas Nussbaum <lucas@debian.org>
2019-05-08 21:05:37 +0200

    Add contrib-buster image

---
+++ packer-virtualbox-vagrant/scripts/vb-guest-additions-buster.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -e
+
+echo "installing virtualbox guest additions"
+apt-get --yes install linux-headers-amd64 devscripts
+
+VBOXVER=$(rmadison --suite unstable virtualbox-guest-dkms \
+	| awk -F'|' '{print $2}' \
+	| sed s/\ //g | head -1)
+
+td=$(mktemp -d)
+cd $td
+wget http://ftp.debian.org/debian/pool/contrib/v/virtualbox/virtualbox-guest-dkms_${VBOXVER}_all.deb
+wget http://ftp.debian.org/debian/pool/contrib/v/virtualbox/virtualbox-guest-utils_${VBOXVER}_amd64.deb
+dpkg -i *deb || true
+apt-get --yes -f --no-install-recommends install
+
+apt-get --yes remove --auto-remove linux-headers*-amd64 devscripts
+apt-get --yes autoremove
+apt-get --yes clean
+cd /
+rm -rf $td
